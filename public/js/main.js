b1c38728397cb48a12cce8fb33cf9fa987b65c5a
vipyne <vipyne@gmail.com>
2014-07-11 21:52:31 -0400

    Create sticky footer with js.
    animation is really wonky.  also only adjusts on window size
    change and not on initial load...  so it's buggy, but at
    least that constant scroll bar is gone.

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,55 @@
 $(document).ready(function(){
+console.log('hi nerds')
 /////////////////////////////
 
+
+// A L L ////////////////////
+/////////////////////////////
+/////////////////////////////
+
+// $(window).bind("load", function() {
+  var $footer, footerTop, footerHeight, bodyHeight
+
+  footerPosition()
+
+  function footerPosition(){
+    $footer = $('footer')
+    footerTop = $(window).scrollTop() + $(window).height() - footerHeight + 'px'
+    footerHeight = $('footer').height()
+    bodyHeight = $('.all-wrap').height()
+
+    // debugger
+
+    if($(window).height() > bodyHeight + footerHeight){
+      $footer.css({
+        position: 'absolute'
+      }).animate({
+        top: footerTop
+      })
+    }else{
+      $footer.css({
+        position: 'static'
+      })
+    }
+  }
+
+  $(window)
+    .scroll(footerPosition)
+    .resize(footerPosition)
+// })
+
+
+// I N D E X ////////////////
+/////////////////////////////
+/////////////////////////////
+
+
+
+// C O D E //////////////////
+/////////////////////////////
+/////////////////////////////
 var scale = 200
 var images = $('.small-pic')
-console.log('images', images)
 
 $.each(images, function(index, image){
   var w = image.offsetWidth
@@ -12,15 +58,14 @@ $.each(images, function(index, image){
   var newHeight = (h * scale) / w
   image.style.width = newWidth + 'px'
   image.style.height = newHeight + 'px'
-  console.log('w', w)
-  console.log('w', w)
-  console.log('newWidth', newWidth)
-  console.log('newHeight', newHeight)
 })
-var size = $('.small-pic').css('width')
-console.log('size', size)
+// var size = $('.small-pic').css('width')
 
 
+
+// R E E L //////////////////
+/////////////////////////////
+/////////////////////////////
 
 
 /////////////////////////////
