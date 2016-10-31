73ec0eb187252ba4f3e9fed90fa698ba179c8104
vipyne <vipyne@gmail.com>
2016-10-31 13:30:51 -0400

    ha log dots

--- js/main.js
+++ js/main.js
@@ -13,6 +13,68 @@ console.log('function hiNerds')
 /////////////////////////////
 /////////////////////////////
 
+// var c=document.getElementById("myCanvas");
+// var ctx=c.getContext("2d");
+
+// ctx.font="20px Georgia";
+// ctx.fillText("Hello World!",10,50);
+
+// ctx.font="30px Verdana";
+// // Create gradient
+// var gradient=ctx.createLinearGradient(0,0,c.width,0);
+// gradient.addColorStop("0","magenta");
+// gradient.addColorStop("0.5","blue");
+// gradient.addColorStop("1.0","red");
+// // Fill with gradient
+// ctx.fillStyle=gradient;
+// ctx.fillText("Big smile!",10,90);
+
+// ctx.beginPath();
+//                     ctx.arc(x,y, radius, 0, 2 * Math.PI, false);
+//                     ctx.clip();
+//                     ctx.drawImage(img,0,0,
+
+var textNameCanvas = document.createElement('canvas');
+textNameCanvas.width = 300;
+textNameCanvas.height = 300;
+
+var textNameCanvasContext = textNameCanvas.getContext('2d');
+
+var dotMatrix = textNameCanvasContext;
+
+
+var rows = 5;
+var cols = 10
+
+
+
+for (var i = 1; i < rows; i++) {
+  for (var j = 1; j < cols; j++) {
+    var center = [textNameCanvas.width/j, textNameCanvas.height/i];
+    dotMatrix.moveTo(center[0], center[1]);
+    console.log('center', center);
+    dotMatrix.arc(center[0], center[1], 10, 0, (2 * Math.PI), false);
+    dotMatrix.fill();
+  }
+}
+
+console.log('derp')
+
+// textNameCanvasContext.font = '30px Arial';
+// dotMatrix.beginPath();
+// dotMatrix.arc(10, 10, 7.5, 0, (2 * Math.PI), false);
+// dotMatrix.fillStyle = '#a5251c';
+// // dotMatrix.fillcolor('rgb(165, 37, 28)');
+// dotMatrix.fill();
+// // dotMatrix.fillStyle(dotMatrix);
+
+document.getElementsByClassName('main')[0].append(textNameCanvas);
+
+// L I N K S ////////////////
+/////////////////////////////
+/////////////////////////////
+
+// todo: site source
 var addEvent = function(theEvent, element, func){
   if(element.addEventListener){
     element.addEventListener(theEvent, func, false)
@@ -20,43 +82,8 @@ var addEvent = function(theEvent, element, func){
     element.attachEvent('on' + theEvent, func)
   }
 }
-
-/////
-
-// var footer = (function(){
-
-//   var $footer, footerHeight, pageHeight
-
-//   var position = function(){
-//     $footer = $('.social-media')
-//     footerHeight = $('.social-media').height()
-//     // pageHeight = $('.all-wrap').height() + 141 // need to figure out why
-//     // this doesn't work on page load...
-//     if($(window).height() > $('.all-wrap').height() + 141 + footerHeight){
-//       $footer.css({
-//         position: 'absolute'
-//       }).animate({
-//         top: $(window).scrollTop() + $(window).height() - footerHeight + 'px'
-//       }, 1)
-//     }else{
-//       $footer.css({
-//         position: 'static'
-//       })
-//     }
-//   }
-
-//   return {
-//     position: position
-//   }
-
-// })()
-
-/////
-
 var hover = (function(){
-
   var host = ''
-
   var afterSrc = function(icon){
     var hover
     var id = icon.getAttribute('id')
@@ -112,13 +139,6 @@ var hover = (function(){
 
 })()
 
-/////
-
-// footer.position()
-// $(window)
-//   .scroll(footer.position)
-//   .resize(footer.position)
-
 hover.allIcons()
 
 // I N D E X ////////////////
@@ -140,7 +160,6 @@ hover.allIcons()
 var html5video = function(){
   return !!document.createElement('video').canPlayType
 }
-
 if(!html5video()){
   var v = $('.video-reel')
   var fallbackLink = document.createElement('p')
