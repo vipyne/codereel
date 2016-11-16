0fa5de7aac379f56155b5c1a9d96b4128172e46a
vipyne <vipyne@gmail.com>
2016-11-15 20:56:10 -0500

    closer... still need to pass in variable from markup

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 // <script type="text/javascript" src="githubbbbb" initials="<your initials here>"></script>
-var vip = 'mvp';
+// var initials = vip;
 
-var all = (function(window, document, undefined){
+var all = (function(window, document, undefined) {
+/////////////////////////////
+// console.log('asdf', document.getElementsByTagName('initials')[0]);
+var vip = 'wer';
+// var vip = initials;
 
 var scale = 1;
 var sWidth = 435 / scale;
@@ -10,7 +14,6 @@ var sRadius = 3.5 / scale;
 var sRows = 11 / scale;
 var sCols = 15 / scale;
 var sTextSize = 370 / scale;
-
 
 var textNameCanvas = document.createElement('canvas');
 textNameCanvas.setAttribute('class', 'header-float');
@@ -21,36 +24,27 @@ document.getElementById('header').insertBefore(textNameCanvas, bgInsert);
 textNameCanvas.width = sWidth;
 textNameCanvas.height = sHeight;
 var textNameCanvasContext = textNameCanvas.getContext('2d');
-
 var dotMatrix = textNameCanvasContext;
-
 
 // V P text
 var inputText = vip;
-// var inputText = 'vip';
-// var inputText = document.currentScript.getAttribute('initials');
 var tempText = inputText.split('');
 var inputLength = tempText.length;
 var derpText = tempText.reverse().slice(tempText.length - 3).reverse();
 initialsText = derpText.join('');
 
 var textSize = sTextSize;
-textNameCanvasContext.font = 'bold 300px monospace';
-// textNameCanvasContext.font = 'bold ' + sTextSize + 'px monospace';
+textNameCanvasContext.font = '300px monospace';
 var textWidth = textNameCanvasContext.measureText(initialsText).width;
-console.log('asdf:', textNameCanvasContext.measureText(initialsText));
 textNameCanvas.width = textWidth + 20;
-var rows = 11;
-var cols = 15;
+var rows = 51;
+var cols = 35;
+var newCols = 200;
 var radius = sRadius;
-var newCols = Math.floor(textNameCanvas.width / 20);
 var xIncrement = textNameCanvas.width / newCols;
 var yIncrement = sHeight/rows;
 
 textNameCanvasContext.fillStyle = 'rgba(100, 100, 100, 0.01)'; // super high transparency
-// textNameCanvasContext.fillText('sfsd', xIncrement, sHeight/2);
-console.log('xIncrement', xIncrement)
-console.log('sHeight/2', sHeight/2)
 textNameCanvasContext.font = '300px monospace';
 textNameCanvasContext.textBaseline = "middle";
 textNameCanvasContext.textAlign = "left";
@@ -69,20 +63,18 @@ function pixelUsed(x, y) {
   }
 }
 
-var topColor =
-var bottomColor =
-
+var colorSeed1 = 90;
+var colorSeed2 = 230;
 // iterate and draw dots !
 for (var i = 1; i < rows; i++) {
   for (var j = 1; j < newCols; j++) {
-    var shade = 220 - i * 8; // change gradient fade
-    // each color is 0 - 255       __red__         __green__           __blue__
-    dotMatrix.fillStyle = 'rgb(' + (shade) + ',' + (shade-65) + ',' + (shade+65) + ')';
+    var shade = colorSeed1 - i*5 + colorSeed2 + j/5;
+    var gShade = i*5 - colorSeed2 + j/5;
+    var bShade = colorSeed1 + i*5 + j/5;
+    // each color is 0 - 255      __red__       __green__      __blue__
+    dotMatrix.fillStyle = 'rgb(' + shade + ',' + gShade + ',' + bShade + ')';
     var center = [xIncrement * j, yIncrement * i];
-    // var newRad = (xIncrement/4 > (radius + j/2 - 2)) ? xIncrement/4 : (radius + j/2 - 2);
-    // var newRad = radius + 5;
-    var newRad = radius + (newCols-3)/3 - 2;
-    // var newRad = radius + j/3 - 2;
+    var newRad = radius - 2;
     if (pixelUsed(center[0], center[1])) {
       dotMatrix.beginPath(); // this effects rasteration... its interesting
       dotMatrix.moveTo(center[0], center[1]);
@@ -92,10 +84,5 @@ for (var i = 1; i < rows; i++) {
     }
   }
 }
-
-
-// textNameCanvasContext.globalCompositeOperation='destination-over';
-// textNameCanvasContext.fillStyle = '#00321e';
-// textNameCanvasContext.fillRect(0, 0, textNameCanvas.width, sHeight);
 /////////////////////////////
-})(window, document)
+})(window, document);
