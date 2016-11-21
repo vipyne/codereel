75153f6531deb0d9828014097f1e92becc032b5d
vipyne <vipyne@gmail.com>
2016-11-21 13:06:35 -0500

    update for my name to make pngs

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var vip = script.getAttribute('initials');
 
 var scale = 1;
 var sWidth = 435 / scale;
-var sHeight = 220 / scale;
+var sHeight = 90 / scale;
 var sRadius = 3.5 / scale;
 var sRows = 11 / scale;
 var sCols = 15 / scale;
@@ -23,25 +23,27 @@ var textNameCanvasContext = textNameCanvas.getContext('2d');
 var dotMatrix = textNameCanvasContext;
 
 // V P text
-var inputText = vip;
-var tempText = inputText.split('');
-var inputLength = tempText.length;
-var derpText = tempText.reverse().slice(tempText.length - 3).reverse();
-initialsText = derpText.join('');
+// var inputText = vip;
+// var tempText = inputText.split('');
+// var inputLength = tempText.length;
+// var derpText = tempText.reverse().slice(tempText.length - 3).reverse();
+// initialsText = derpText.join('');
+initialsText = vip;
 
 var textSize = sTextSize;
-textNameCanvasContext.font = '300px monospace';
+textNameCanvasContext.font = '54px bold monospace';
 var textWidth = textNameCanvasContext.measureText(initialsText).width;
-textNameCanvas.width = textWidth + 20;
-var rows = 51;
-var cols = 35;
-var newCols = 200;
+console.log('textWidth', textWidth)
+textNameCanvas.width = textWidth + 120;
+var rows = 37;
+var cols = 15;
+var newCols = 126;
 var radius = sRadius;
 var xIncrement = textNameCanvas.width / newCols;
 var yIncrement = sHeight/rows;
 
 textNameCanvasContext.fillStyle = 'rgba(100, 100, 100, 0.01)'; // super high transparency
-textNameCanvasContext.font = '300px monospace';
+textNameCanvasContext.font = '54px bold monospace';
 textNameCanvasContext.textBaseline = "middle";
 textNameCanvasContext.textAlign = "left";
 textNameCanvasContext.fillText(initialsText.toUpperCase(), xIncrement, sHeight/2);
@@ -67,12 +69,14 @@ for (var i = 1; i < rows; i++) {
     var shade = colorSeed1 - i*5 + colorSeed2 + j/5;
     var gShade = i*5 - colorSeed2 + j/5;
     var bShade = colorSeed1 + i*5 + j/5;
-    // each color is 0 - 255      __red__       __green__      __blue__
-    dotMatrix.fillStyle = 'rgb(' + shade + ',' + gShade + ',' + bShade + ')';
+    // // each color is 0 - 255      __red__       __green__      __blue__
+    // dotMatrix.fillStyle = 'rgb(' + shade + ',' + gShade + ',' + bShade + ')';
+    dotMatrix.fillStyle = 'rgb(1, 1, 1)';
     var center = [xIncrement * j, yIncrement * i];
-    var newRad = radius - 2;
+    console.log('asdfas', (radius - 2 -.3)*(i/8));
+    var newRad = ( ((radius - 2 -.3)*(i/24) ) > 1.9) ? 1.9 : (radius - 2 -.3)*(i/24);
     if (pixelUsed(center[0], center[1])) {
-      dotMatrix.beginPath(); // this effects rasteration... its interesting
+      // dotMatrix.beginPath(); // this effects rasteration... its interesting
       dotMatrix.moveTo(center[0], center[1]);
       dotMatrix.arc(center[0], center[1], newRad, 0, 2 * Math.PI, false);
       dotMatrix.fill();
